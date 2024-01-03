8e5399e71b9ac716a79c180dccbf8023ec692993
manascb1344 <zephop76593@gmail.com>
2024-01-03 15:02:19 +0530

    Added Array CF Jan 3rd

--- 03.01.2024/Array.cpp
+++ 03.01.2024/Array.cpp
@@ -1,3 +1,5 @@
+// https://codeforces.com/contest/300/problem/A
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -12,11 +14,60 @@ int main()
     int n;
     cin >> n;
 
-
+    vi f, s, t;
 
     for (int i = 0; i < n; i++)
     {
+        int tmp;
+        cin >> tmp;
+        if (tmp < 0)
+        {
+            f.push_back(tmp);
+        }
+
+        if (tmp > 0)
+        {
+            s.push_back(tmp);
+        }
+        if (tmp == 0)
+        {
+            t.push_back(tmp);
+        }
     }
 
+    if (s.empty())
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            s.push_back(f.back());
+            f.pop_back();
+        }
+    }
+
+    if (f.size() % 2 == 0)
+    {
+        t.push_back(f.back());
+        f.pop_back();
+    }
+
+    cout << f.size();
+    for (int val : f)
+    {
+        cout << " " << val;
+    }
+    cout << "\n";
+
+    cout << s.size();
+    for (int val : s)
+    {
+        cout << " " << val;
+    }
+    cout << "\n";
+
+    cout << t.size();
+    for (int val : t)
+    {
+        cout << " " << val;
+    }
     return 0;
 }
