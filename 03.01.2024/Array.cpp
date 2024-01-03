415c50cfb9fce057973b7f729b51ae235d0a271f
manascb1344 <zephop76593@gmail.com>
2024-01-03 14:47:58 +0530

    Done Array CF 3 Jan

---
+++ 03.01.2024/Array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+
+
+    for (int i = 0; i < n; i++)
+    {
+    }
+
+    return 0;
+}
---
+++ 03.01.2024/Arrays.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int na, nb;
+    cin >> na >> nb;
+    int k, m;
+    cin >> k >> m;
+    int an[na];
+    int bn[nb];
+    for (int i = 0; i < na; i++)
+    {
+        cin >> an[i];
+    }
+    for (int i = 0; i < nb; i++)
+    {
+        cin >> bn[i];
+    }
+
+    if (an[k - 1] < bn[nb - m])
+    {
+        cout << "YES"
+             << "\n";
+    }
+    else
+    {
+        cout << "NO"
+             << "\n";
+    }
+
+    return 0;
+}
