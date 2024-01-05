3d3af4bbfc9e82126998256e28673ca69717421f
manascb1344 <zephop76593@gmail.com>
2024-01-05 23:38:54 +0530

    Done Songs Compression 4th Jan

--- 04.01.2024/Songs_Compression.cpp
+++ 04.01.2024/Songs_Compression.cpp
@@ -11,13 +11,37 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int tc;
-	cin >> tc;
-	while (tc--)
+	ll ans = 0;
+
+	int n, m;
+	cin >> n >> m;
+	vector<ll> a;
+	ll suma = 0, sumb = 0;
+
+	for (ll i = 0; i < n; i++)
 	{
-		
+		ll tmp1, tmp2;
+		cin >> tmp1 >> tmp2;
+		a.pb(tmp1 - tmp2);
+		suma += tmp1;
+	}
+	sort(a.begin(), a.end());
+	reverse(a.begin(), a.end());
 
+	while (suma > m && ans < n)
+	{
+		suma -= a[ans];
+		ans += 1;
+	}
 
+	if (suma <= m)
+	{
+		cout << ans << "\n";
+	}
+	else
+	{
+		cout << "-1"
+				 << "\n";
 	}
 
 	return 0;
