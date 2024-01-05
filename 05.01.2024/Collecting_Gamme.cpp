dc1124a49bcbef39eb072b8644d46f61007b59c7
manascb1344 <zephop76593@gmail.com>
2024-01-05 22:41:03 +0530

    Done minimum-moves-to-equal-array-elements 5th Jan

---
+++ 05.01.2024/Collecting_Gamme.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/1904/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		
+
+
+	}
+
+	return 0;
+}
