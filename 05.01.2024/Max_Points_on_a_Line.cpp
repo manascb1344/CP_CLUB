06df3929624104577f6a5f058a611243cf535454
manascb1344 <zephop76593@gmail.com>
2024-01-05 22:53:20 +0530

    Done Max Points on a Line 5th Jan

--- 05.01.2024/Max_Points_on_a_Line.cpp
+++ 05.01.2024/Max_Points_on_a_Line.cpp
@@ -1,24 +1,28 @@
 // https://leetcode.com/problems/max-points-on-a-line/
 
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define pb push_back
-#define vi vector<int>
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans = 0;
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+        if(n==1){
+            return 1;
+        }
 
-	int tc;
-	cin >> tc;
-	while (tc--)
-	{
-		
-
-
-	}
-
-	return 0;
-}
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int sol = 0;
+                for(int k = 0; k < n; k++){
+                    int s1 = (points[k][1] - points[i][1]) * (points[j][0] - points[i][0]);
+                    int s2 = (points[k][0] - points[i][0]) * (points[j][1] - points[i][1]);
+                    if(s1 == s2){
+                        sol++;
+                    }
+                }
+                ans=max(ans,sol);
+            }
+        }
+        return ans;
+    }
+};
