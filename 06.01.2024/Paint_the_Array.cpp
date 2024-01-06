9742f65c0c21ba37580dfdaa84d8c7d7badcd5c1
manascb1344 <zephop76593@gmail.com>
2024-01-06 23:46:41 +0530

    Done paint the Array 6th Jan

--- 06.01.2024/Paint_the_Array.cpp
+++ 06.01.2024/Paint_the_Array.cpp
@@ -6,6 +6,59 @@ using namespace std;
 #define pb push_back
 #define vi vector<int>
 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    ll gcd1 = 0, gcd2 = 0;
+    for (int i = 0; i < n; i += 2)
+    {
+        gcd1 = __gcd(gcd1, a[i]);
+    }
+    for (int i = 1; i < n; i += 2)
+    {
+        gcd2 = __gcd(gcd2, a[i]);
+    }
+
+    bool flag = true;
+    for (int i = 0; i < n; i += 2)
+    {   
+        if (a[i] % gcd2 == 0)
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+    {
+        cout << gcd2 << "\n";
+        return;
+    }
+
+    flag = true;
+    for (int i = 1; i < n; i += 2)
+    {
+        if (a[i] % gcd1 == 0)
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+    {
+        cout << gcd1 << "\n";
+        return;
+    }
+
+    cout << 0 << "\n";
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -15,57 +68,7 @@ int main()
     cin >> tc;
     while (tc--)
     {
-        int n;
-        cin >> n;
-        vi a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        int gcd1 = 0;
-        int gcd2 = 0;
-        for (int i = 0; i < n; i += 2)
-        {
-            gcd1 = __gcd(gcd1, a[i]);
-        }
-        for (int i = 1; i < n; i += 2)
-        {
-            gcd2 = __gcd(gcd2, a[i]);
-        }
-
-        bool flag = true;
-        for (int i = 0; i < n; i += 2)
-        {
-            if (a[i] % gcd2 == 0)
-            {
-                flag = false;
-                break;
-            }
-        }
-        if (flag)
-        {
-            cout << gcd2 << "\n";
-        }
-        else
-        {
-            flag = true;
-            for (int i = 1; i < n; i += 2)
-            {
-                if (a[i] % gcd1 == 0)
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag)
-            {
-                cout << gcd1 << "\n";
-            }
-            else
-            {
-                cout << 0 << "\n";
-            }
-        }
+        solve();
     }
 
     return 0;
