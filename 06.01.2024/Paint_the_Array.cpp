9446ab1925ead123d0bd70501ab341c9c09a6102
manascb1344 <zephop76593@gmail.com>
2024-01-06 23:35:18 +0530

    Done paint the Array

--- 06.01.2024/Paint_the_Array.cpp
+++ 06.01.2024/Paint_the_Array.cpp
@@ -8,17 +8,65 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int tc;
-	cin >> tc;
-	while (tc--)
-	{
-		
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vi a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int gcd1 = 0;
+        int gcd2 = 0;
+        for (int i = 0; i < n; i += 2)
+        {
+            gcd1 = __gcd(gcd1, a[i]);
+        }
+        for (int i = 1; i < n; i += 2)
+        {
+            gcd2 = __gcd(gcd2, a[i]);
+        }
 
+        bool flag = true;
+        for (int i = 0; i < n; i += 2)
+        {
+            if (a[i] % gcd2 == 0)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            cout << gcd2 << "\n";
+        }
+        else
+        {
+            flag = true;
+            for (int i = 1; i < n; i += 2)
+            {
+                if (a[i] % gcd1 == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                cout << gcd1 << "\n";
+            }
+            else
+            {
+                cout << 0 << "\n";
+            }
+        }
+    }
 
-	}
-
-	return 0;
+    return 0;
 }
